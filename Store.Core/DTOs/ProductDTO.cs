364ac445fc7cd6d8dcadf44663e1abd48e9dbd9f
OmarLighthouse <52006093+Cj72001@users.noreply.github.com>
2025-10-30 11:24:38 -0600

    Add: DTOs

---
+++ Store.Core/DTOs/ProductDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Core.DTOs
+{
+    public  class ProductDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Stock { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
